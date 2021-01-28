17b971bbd912c4429583f6f360eef287782d86f7
hassan rao <hassanribery0335@gmail.com>
2021-01-28 16:00:49 +0500

    posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function MediaCard({title,userImage,userPost,body}){
+  return(
+    <div className="card">
+      <img src={userImage} width='50px'/>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <img src={userPost} width='500px'/>
+    </div>
+  )
+}
+
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1>Posts</h1>
+     <MediaCard title="Hassan" userImage='https://cdn.iconscout.com/icon/free/png-512/laptop-user-1-1179329.png' userPost="https://wallpaperaccess.com/full/671267.jpg" body="Playing Dota 2" />
+     <MediaCard title="Hassan" userImage='https://cdn.iconscout.com/icon/free/png-512/laptop-user-1-1179329.png' userPost="https://wallpaperaccess.com/full/671267.jpg" body="Playing Dota 2" />
+     <MediaCard title="Hassan" userImage='https://cdn.iconscout.com/icon/free/png-512/laptop-user-1-1179329.png' userPost="https://wallpaperaccess.com/full/671267.jpg" body="Playing Dota 2" />
     </div>
   );
 }
