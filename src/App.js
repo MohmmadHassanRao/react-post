243ea2b86de67d934cc211da6de4af96bb8d30ee
hassan <hassanribery0335@gmail.com>
2021-01-29 18:08:43 +0500

    asignment 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import './App.css';
+
+ 
 
 
 function MediaCard({title,userImage,userPost,body}){
@@ -13,10 +16,22 @@ function MediaCard({title,userImage,userPost,body}){
 }
 
 function App() {
-
+  
+  const [isLit,setLit] = useState(true)
+  const [counter,setCounter] = useState(0)
   
   return (
-    <div className="App">
+    <div  className={isLit?"bright App":"dark App"}>
+    <div>
+      <button onClick={(prevValue)=>setCounter(counter-1)}>-</button>
+      <span>  {counter}  </span>
+      <button onClick={(prevValue)=>setCounter(counter+1)}>+</button>
+
+    </div>
+    <div >
+      <h2 >the room is {isLit?"bright":"dark"}</h2>
+      <button onClick={(prevState)=>{setLit(!isLit)}}>Change</button>
+    </div>
             <h1>Posts</h1>
      <MediaCard title="Hassan" userImage='https://cdn.iconscout.com/icon/free/png-512/laptop-user-1-1179329.png' userPost="https://wallpaperaccess.com/full/671267.jpg" body="Playing Dota 2" />
      <MediaCard title="Hassan" userImage='https://cdn.iconscout.com/icon/free/png-512/laptop-user-1-1179329.png' userPost="https://wallpaperaccess.com/full/671267.jpg" body="Playing Dota 2" />
